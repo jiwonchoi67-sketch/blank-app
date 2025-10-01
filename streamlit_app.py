dd0c70bcfe38ced9c461e6d4144ca666b55cb0a5
jiwonchoi67-sketch <jiwonchoi67@goedu.kr>
2025-10-01 09:14:09 +0000

    나의 첫 번쨰 앱

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈최지원의 첫번쨰 앱!")
+
+st.success("티라미수를 좋아해요")
+st.info("티라미수를 좋아해요")
+st.image("https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA2MDhfMTkg%2FMDAxNzE3ODU3NzcwODE2.ikKYGncg1ZvGZhOhqM8dGba_N81_YaUJxilo44o6-78g.QuY_uR6YuiShvsoVty1Bv3lcP3TLOYVazqR_L1j6oy8g.JPEG%2FIMG_4144.JPG&type=sc960_832")
